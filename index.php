aeb04249f8ab30a64e42ea181e41799cb8367896
Van-Gunt Allen <vanguntallen@gmail.com>
2021-04-18 10:32:07 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,258 @@
+
+<!DOCTYPE html>
+
+<html lang="en" itemscope="" itemtype="">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+ <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+ <style>   
+vga-video_player { 
+   width: 100%; 
+   height: auto; 
+ } 
+</style>
+ 
+<style>
+ * { margin: 0; padding: ; }
+  body {
+font: 16px/1.4 Georgia, Serif;
+  background: url();
+ }
+ #page-wrap {
+	width: 50%;
+ margin: 0px auto;
+ }
+ h1 { font-weight: normal; font-size: 42px; }
+  h1, p, pre, video, h2, figure, h3, ol, script, style { margin: 0 0 0px 0; }
+ h2 { margin-top: 0px; }
+ h1 { margin-bottom: 40px; }
+ li { margin: 0 0 5px 20px; }
+ article { background: white; padding: 10%; }
+ pre, article style, article script {
+ 	white-space: pre;
+	display: block;
+	padding: 10px;
+	background: #eee;
+overflow-x: auto;
+font: 12px Monaco, MonoSpace;
+  }
+ img { max-width: 100%; }
+ figure { display: block; background: #ffffff; padding: 10px; }
+  figcaption { display: block; text-align: center; margin: 0px;  font-style: italic; font-size: 14px; orphans: 2; border: 0; }
+
+ </style>
+</style>
+    <style scoped>video {
+  width: 100%    !important;
+  height: auto  !important;
+}</style>
+  <link rel="stylesheet" href="playerfun-8.css">
+   <script src="https://vga.smtvs.com/js/playerfun.js"></script>
+  
+ 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+<style>
+table { 
+  width: 80%; 
+  border-collapse: 0%; 
+}
+/* Zebra striping */
+tr:nth-of-type(odd) { 
+  background: #fff; 
+}
+th { 
+  background: #fff; 
+  color: white; 
+  font-weight: bold; 
+}
+td, th { 
+  padding: 6px; 
+  border: 1px solid #ccc; 
+  text-align: left; 
+}
+</style>
+
+</head>
+
+<main>
+	
+	<header>
+
+<figure id="vga-video_player" ><center>
+     
+<table class="center">
+ 
+  <tr>
+    <td>
+
+
+<div id="video_container" >
+<video  id="my_video" preload="metadata" playsinline controls   style="  height:450px;">
+<source src="video/VID_20210203_145935.mp4" type="video/mp4">
+<source src="videos/VID_20210203_145935.webm" type="video/webm">
+
+ 
+</video>
+<div id="video_controls_bar"  style="font-size: 10px; font-family: arial, verdana, helvetica, sans serif; margin-left: 1px; color:#ffffff;">
+&nbsp;&nbsp; <button id="playpausebtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
+  <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>
+</svg></button>
+  &nbsp;
+<span id="curtimetext">00:00</span> 
+<input id="seekslider" type="range" min="0" max="100" value="0" step="1" >
+&nbsp;
+ <button id="mutebtn"><i class ='fa fa-volume-up'></i></button>
+&nbsp;&nbsp;
+
+  <input id="volumeslider" type="range" min="0" max="100" value="100" step="1">
+&nbsp;
+&nbsp;&nbsp;
+
+ <button id="fullscreenbtn" > 
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"/>
+</svg>
+</button> &nbsp;
+ <p>
+        <button id="fullscreenbtn"><img src="./CTV Tube-home_files/logo.png" width="70" height="20" alt="" class="img-responsive"></button>&nbsp;&nbsp;
+              </p>
+ </div>
+ </div>
+</td>
+    <td  >
+<div >
+  
+
+<figcaption id="playlist"   align="rihgt" >
+
+ 
+  
+<a href="/video/VID_20210110_151337 (1).mp4" class="currentvid">
+		<img src="cooking.png" width="200" class="img-responsive">		
+<time datetime="&gt;2021-02-13" class="duration">00:13</time>
+<p style="font-size: 10px; font-family: arial, verdana, helvetica, sans serif; margin-left: 1px; color:#000;"class="f-title"> I Grade Cooking ,...</p></a>
+
+   
+<a href="video/VID_20210203_145935.mp4" class="currentvid">
+<img src="builder.png" width="200" class="img-responsive">	
+<time datetime="&gt;2021-02-13" class="duration">00:13</time>
+<p style="font-size: 10px; font-family: arial, verdana, helvetica, sans serif; margin-left: 1px; color:#000;"class="f-title">Van-Gunt Allen IT Dev Pro born in Kingston Jamaica grow up in the USA Independent ,,...</p></a>
+
+
+ 
+  
+<a href="/video/VID_20210110_151337 (1).mp4" class="currentvid">
+		<img src="cooking.png" width="200" class="img-responsive">		
+<time datetime="&gt;2021-02-13" class="duration">00:13</time>
+<p style="font-size: 10px; font-family: arial, verdana, helvetica, sans serif; margin-left: 1px; color:#000;"class="f-title"> I Grade Cooking ,...</p></a>
+
+   
+<a href="video/VID_20210203_145935.mp4" class="currentvid">
+<img src="builder.png" width="200" class="img-responsive">	
+<time datetime="&gt;2021-02-13" class="duration">00:13</time>
+<p style="font-size: 10px; font-family: arial, verdana, helvetica, sans serif; margin-left: 1px; color:#000;"class="f-title">Van-Gunt Allen IT Dev Pro born in Kingston Jamaica grow up in the USA Independent ,,...</p></a>
+
+
+
+</figcaption>
+</div>
+
+</div></div>
+
+</meter>	</header>
+	    
+
+<script>
+var vga_player = document.getElementById("vga-video_player");
+video = vga_player.getElementsByTagName("video")[0],
+video_links = vga_player.getElementsByTagName("figcaption")[0],
+source = video.getElementsByTagName("source"),
+link_list = [],
+path = 'video/',
+currentVid = 0,
+allLnks = video_links.children,
+lnkNum = allLnks.length;
+video.removeAttribute("controls");
+video.removeAttribute("poster");
+
+(function() {
+function playVid(index) {
+    video_links.children[index].classList.add("currentvid");
+    source[1].src = path + link_list[index] + ".webm";  
+	source[0].src = path + link_list[index] + ".mp4";
+    currentVid = index;
+    video.load();
+	video.play();
+}
+
+for (var i=0; i<lnkNum; i++) {
+var filename = allLnks[i].href;
+link_list[i] = filename.match(/([^\/]+)(?=\.\w+$)/)[0];
+(function(index){
+        allLnks[i].onclick = function(i){
+        i.preventDefault();  
+		for (var i=0; i<lnkNum; i++) {
+		allLnks[i].classList.remove("currentvid");
+		}
+		playVid(index);
+        }    
+    })(i);
+}
+video.addEventListener('ended', function () {
+    allLnks[currentVid].classList.remove("currentvid");
+    if ((currentVid + 1) >= lnkNum) { nextVid = 0 } else { nextVid = currentVid+1 }
+    playVid(nextVid);
+})
+
+
+
+
+var indexOf = function(needle) {
+    if(typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1, index = -1;
+			for(i = 0; i < this.length; i++) {
+                if(this[i] === needle) {
+                    index = i;
+                    break;
+                }}
+            return index;
+        };}
+    return indexOf.call(this, needle);
+};
+ 	var focusedLink = document.activeElement;
+ 	index = indexOf.call(allLnks, focusedLink);
+ 	
+document.addEventListener('keydown', function(e) {
+if (index) {
+	var focusedElement = document.activeElement;
+    if (e.keyCode == 40 || e.keyCode == 39) { // down or right cursor
+    var nextNode = focusedElement.nextElementSibling;
+    if (nextNode) { nextNode.focus(); } else { video_links.firstElementChild.focus(); }
+    }
+   if (e.keyCode == 38 || e.keyCode == 37) { // up or left cursor
+    var previousNode = focusedElement.previousElementSibling;
+    if (previousNode) { previousNode.focus(); } else { video_links.lastElementChild.focus(); }
+    }
+ }
+});
+
+})();  
+</script>
+
+
+</main>
+
+
+
+</body>
+
+
+
+
+</html>
